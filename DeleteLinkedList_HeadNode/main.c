c8b4f32484273e896e4d367de1623b65f97211c8
chenyufeng <503434355@qq.com>
2016-03-01 15:11:46 +0800

    Initial

--- DeleteLinkedList_HeadNode/main.c
+++ DeleteLinkedList_HeadNode/main.c
@@ -6,10 +6,15 @@
 //  Copyright © 2016年 chenyufengweb. All rights reserved.
 //
 
+/**
+ *  对带头结点的单链表的节点进行删除
+ （1）删除某个位置i的节点；
+ （2）判断x值是否在链表中，若存在则删除该节点；
+ */
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
     return 0;
 }
